8bff04b804c38fb09c3e5b9b89f370b63c2eaf96
Idris Attar <idrisattar@live.com>
2022-05-10 20:06:04 +0530

    variant test

--- src/app/data/project.ts
+++ src/app/data/project.ts
@@ -4,41 +4,34 @@ export const project = [
     name: 'Tech-Hub',
     image: require('app/data/images/techhub/techHub.png'),
     details: {
-      image: require('app/data/images/techhub/full.png'),
+      image: require('app/data/images/techhub/techHub.png'),
       visibility: false,
       topics: [
         {
           // topic: 'The Task',
-          description: `
-          An electronics retailer's app and website were due for an update. Sales were going up the roof, and customers were getting a little antsy with the old-school interface, begging for something that would make their shopping experience easier and more engaging. The owners weren't afraid to try a new style or to break some rules—they had been in business for years and had seen trends come and go without losing any of their loyal customers.
-
-Input from focus groups showed that people liked the store's selection of big-ticket items, but they also wanted to be able to find what they needed quickly. They wanted a shop that offered the best deals on electronics, with the best quality products. They wanted to have access to knowledgeable salespeople who could answer any questions about products or help them choose one that was right for them.
-
-The owners knew they had loyal customers, so they decided to ask if they wanted anything else—and they got an earful! People wanted access to online tutorials so they could learn how to use the things they bought; they wanted a forum where others had already asked their questions so there would be no guesswork; they wanted a place
-An electronics retailer's app and website were due for an update. Sales were going up the roof, and customers were getting a little antsy with the old-school interface, begging for something that would make their shopping experience easier and more engaging. The owners weren't afraid to try a new style or to break some rules—they had been in business for years and had seen trends come and go without losing any of their loyal customers.
-
-Input from focus groups showed that people liked the store's selection of big-ticket items, but they also wanted to be able to find what they needed quickly. They wanted a shop that offered the best deals on electronics, with the best quality products. They wanted to have access to knowledgeable salespeople who could answer any questions about products or help them choose one that was right for them.
-An electronics retailer's app and website were due for an update. Sales were going up the roof, and customers were getting a little antsy with the old-school interface, begging for something that would make their shopping experience easier and more engaging. The owners weren't afraid to try a new style or to break some rules—they had been in business for years and had seen trends come and go without losing any of their loyal customers.
-
-Input from focus groups showed that people liked the store's selection of big-ticket items, but they also wanted to be able to find what they needed quickly. They wanted a shop that offered the best deals on electronics, with the best quality products. They wanted to have access to knowledgeable salespeople who could answer any questions about products or help them choose one that was right for them.
-          `,
-          image: require('app/data/images/techhub/full.png'),
-          position: 'top',
+          description: `An electronics retailer's app and website were due for an update. Sales were going up the roof, and customers were getting a little antsy with the old-school interface, begging for something that would make their shopping experience easier and more engaging. The owners weren't afraid to try a new style or to break some rules—they had been in business for years and had seen trends come and go without losing any of their loyal customers.`,
+          image: require('app/data/images/techhub/p1.png'),
+          position: 'bottom',
         },
-        // {
-        //   topic: 'My Role and Responsibilities',
-        //   description:
-        //     'As UI/UX Designer it was my responsibility to keep the design elements and the functionality of the application and website clean and modern',
-        //   image: require('app/data/images/techhub/techHub1.png'),
-        //   position: 'left',
-        // },
-        // {
-        //   topic: 'Inspiration + Process',
-        //   description:
-        //     'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam',
-        //   image: require('app/data/images/techhub/techHub1.png'),
-        //   position: 'bottom',
-        // },
+        {
+          description: `
+          Input from focus groups showed that people liked the store's selection of big-ticket items, but they also wanted to be able to find what they needed quickly. They wanted a shop that offered the best deals on electronics, with the best quality products. They wanted to have access to knowledgeable salespeople who could answer any questions about products or help them choose one that was right for them.
+          `,
+          image: require('app/data/images/techhub/p2.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/techhub/p3.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/techhub/p4.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/techhub/p5.png'),
+          position: 'bottom',
+        },
       ],
     },
   },
