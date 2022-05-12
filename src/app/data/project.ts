997bdb9ac49def434ce88917e08b208dd3905757
Idris Attar <idrisattar@live.com>
2022-05-13 00:28:14 +0530

    project update

--- src/app/data/project.ts
+++ src/app/data/project.ts
@@ -40,28 +40,68 @@ export const project = [
     name: 'Car-Hub',
     image: require('app/data/images/carHub.png'),
     details: {
-      image: require('app/data/images/techhub/techHub1.png'),
-      visibility: true,
+      image: require('app/data/images/carhub/carhub-1.png'),
+      visibility: false,
       topics: [
         {
           topic: 'The Task',
           description:
             'Re-designing application and website to be cleaner and modern but still keeping the same design elements.',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/carhub/carhub-1.png'),
           position: 'bottom',
         },
         {
           topic: 'Inspiration + Process',
           description:
             'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/carhub/carhub-2.png'),
           position: 'bottom',
         },
         {
           topic: 'My Role and Responsibilities',
           description:
             'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam.',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/carhub/carhub-3.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-4.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-5.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-6.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-7.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-8.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-9.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-10.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-11.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-12.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/carhub/carhub-13.png'),
           position: 'bottom',
         },
       ],
@@ -72,28 +112,90 @@ export const project = [
     name: 'Gym-Hub',
     image: require('app/data/images/gym.png'),
     details: {
-      image: require('app/data/images/techhub/techHub1.png'),
-      visibility: true,
+      image: require('app/data/images/gym/Class.png'),
+      visibility: false,
       topics: [
         {
-          topic: 'The Task',
+          description: 'Wireframe of the app',
+        },
+        {
+          topic: 'Spinning Cycle',
           description:
-            'Re-designing application and website to be cleaner and modern but still keeping the same design elements.',
-          image: require('app/data/images/techhub/techHub1.png'),
+            'Select any of the spinning cycle classes and get started with the trainer you prefer.',
+          image: require('app/data/images/gym/SpinningCycle.png'),
           position: 'bottom',
         },
         {
-          topic: 'Inspiration + Process',
+          topic: 'Personal Training',
           description:
-            'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam.',
-          image: require('app/data/images/techhub/techHub1.png'),
+            'Choose your own personal trainer depending on your gender',
+          image: require('app/data/images/gym/PersonalTraining.png'),
           position: 'bottom',
         },
         {
-          topic: 'My Role and Responsibilities',
+          topic: 'Packages',
+          description: 'Purchase a package and with exciting offers',
+          image: require('app/data/images/gym/Packages.png'),
+          position: 'bottom',
+        },
+        {
+          topic: 'Class',
           description:
-            'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam.',
-          image: require('app/data/images/techhub/techHub1.png'),
+            'Zumba, Yoga, Pilates, Kickboxing, and more classes to choose from.',
+          image: require('app/data/images/gym/Class.png'),
+          position: 'bottom',
+        },
+        {
+          topic: 'Events',
+          description:
+            'Join our events alone or with a friend and get the best of the best.',
+          image: require('app/data/images/gym/Events.png'),
+          position: 'bottom',
+        },
+        {
+          topic: 'Miscellaneous',
+          description: 'Change your email, password, and more',
+          image: require('app/data/images/gym/Miscellaneous.png'),
+          position: 'bottom',
+        },
+        {
+          // topic: 'The Task',
+          description: 'Designs for app',
+          // image: require('app/data/images/carhub/carhub-1.png'),
+          // position: 'bottom',
+        },
+        {
+          topic: 'Miscellaneous',
+          description: 'Change your email, password, and more',
+          image: require('app/data/images/gym/gym0.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym1.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym2.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym3.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym4.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym5.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym6.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/gym/gym7.png'),
           position: 'bottom',
         },
       ],
@@ -104,14 +206,14 @@ export const project = [
     name: 'Gold-eCommerce',
     image: require('app/data/images/gold.png'),
     details: {
-      image: require('app/data/images/techhub/techHub1.png'),
+      image: require('app/data/images/gym/Class.png'),
       visibility: true,
       topics: [
         {
           topic: 'The Task',
           description:
             'Re-designing application and website to be cleaner and modern but still keeping the same design elements.',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/gym/Class.png'),
           position: 'bottom',
         },
         {
@@ -136,28 +238,56 @@ export const project = [
     name: 'Football-Trivia',
     image: require('app/data/images/football.png'),
     details: {
-      image: require('app/data/images/techhub/techHub1.png'),
+      image: require('app/data/images/football/football1.png'),
       visibility: true,
       topics: [
         {
           topic: 'The Task',
           description:
             'Re-designing application and website to be cleaner and modern but still keeping the same design elements.',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/football/football1.png'),
           position: 'bottom',
         },
         {
           topic: 'Inspiration + Process',
           description:
-            'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam.',
-          image: require('app/data/images/techhub/techHub1.png'),
+            'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam',
+          image: require('app/data/images/football/football2.png'),
           position: 'bottom',
         },
         {
           topic: 'My Role and Responsibilities',
           description:
             'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia! Quasi, quisquam.',
-          image: require('app/data/images/techhub/techHub1.png'),
+          image: require('app/data/images/football/football3.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football4.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football5.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football6.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football7.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football8.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football9.png'),
+          position: 'bottom',
+        },
+        {
+          image: require('app/data/images/football/football10.png'),
           position: 'bottom',
         },
       ],
